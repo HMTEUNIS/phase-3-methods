72e4f7c8da36b675d8ee4650a87bc8ebb899233a
Holly Teunis <+>
2021-11-22 20:09:08 -0500

    commit tree 9233660820c3f16854408ad856ad6753bda909ce

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
 # Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+
+def greet (name =  "programmer")
+    puts "Hello, #{name}!"
+end
+
+ def greet_with_default (name =  "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add num, num2
+    num + num2
+end
+
+def halve i = 0
+    if i.class != Integer
+        return nil
+    end
+    
+  i.div(2)
+   
+end
